3ebf24b82b9de4ea84f8688c55990eb7bad4b0e5
rohan01011979-cell <rohan01011979@gmail.com>
2026-02-18 14:23:20 +0530

    Add live data configuration for events

---
+++ live-links.js
@@ -0,0 +1,12 @@
+const LIVE_DATA = {
+    "Ujjawal-2025-cgl": {
+        "isLive": true,
+        "title": "Maths: Percentage Live @ 10 AM",
+        "link": "https://tumhara-m3u8-link.m3u8"
+    },
+    "Surya_2026_cgl": {
+        "isLive": false,
+        "title": "Next Live @ 2 PM",
+        "link": ""
+    }
+};

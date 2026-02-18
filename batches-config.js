b2b603c1bea2669c75e78c6f87dce3cce3ef663c
rohan01011979-cell <rohan01011979@gmail.com>
2026-02-18 13:34:30 +0530

    Add remote batch file URLs to batches-config.js

---
+++ batches-config.js
@@ -0,0 +1,4 @@
+const REMOTE_BATCH_FILES = [
+    "https://gist.githubusercontent.com/rohan01011979-cell/2273dda70535a08d18843ebc769641c4/raw/Ujjawal-2025-cgl.js",
+    "https://gist.githubusercontent.com/rohan01011979-cell/1d613b99cfde651c6252acab91a3e479/raw/Surya_2026_cgl.js"
+];
